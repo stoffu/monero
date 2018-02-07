8b8057df7a413d7d60593c29763dcaa764115217
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2018-02-07 19:07:36 +0000

    Monero Cryptonight variants, and add one for v7 /monero#3253
    This is the first variant of many, with the intent to improve
    Monero's resistance to ASICs and encourage mining decentralization.

--- src/crypto/hash.h
+++ src/crypto/hash.h
@@ -71,16 +71,16 @@ namespace crypto {
     return h;
   }
 
-  inline void cn_slow_hash(const void *data, std::size_t length, hash &hash, int light) {
-    cn_slow_hash(data, length, reinterpret_cast<char *>(&hash), light);
+  inline void cn_slow_hash(const void *data, std::size_t length, hash &hash, int light, int variant = 0) {
+    cn_slow_hash(data, length, reinterpret_cast<char *>(&hash), light, variant);
   }
 
-  inline void cn_slow_hash_1m(const void *data, std::size_t length, hash &hash) {
-    cn_slow_hash(data, length, hash, 1);
+  inline void cn_slow_hash_1m(const void *data, std::size_t length, hash &hash, int variant = 0) {
+    cn_slow_hash(data, length, hash, 1, variant);
   }
 
-  inline void cn_slow_hash_2m(const void *data, std::size_t length, hash &hash) {
-    cn_slow_hash(data, length, hash, 0);
+  inline void cn_slow_hash_2m(const void *data, std::size_t length, hash &hash, int variant = 0) {
+    cn_slow_hash(data, length, hash, 0, variant);
   }
 
   inline void tree_hash(const hash *hashes, std::size_t count, hash &root_hash) {
